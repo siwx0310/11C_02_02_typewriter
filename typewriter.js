a353d578cab10d325550cddfe9cb2d2bd60d780f
Siw <siwx0310@stud.kea.dk>
2021-02-10 12:47:25 +0100

    next letter works an resets when done

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,29 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+const getNextLetter = document.querySelector(".typewritten").textContent;
+let n = 0;
+
+function start() {
+  console.log("start");
+
+  document.querySelector(".typewritten").textContent = "";
+
+  nextL();
+}
+
+function nextL() {
+  console.log(nextL);
+
+  if (n < getNextLetter.length) {
+    document.querySelector(".typewritten").textContent += getNextLetter.charAt(
+      n
+    );
+    n++;
+    setTimeout(nextL, 150);
+  } else if (n === getNextLetter.length) {
+    n = 0;
+    setTimeout(start, 10);
+  }
+}
